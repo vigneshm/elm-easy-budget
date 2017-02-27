b7d693bf5a34afa60dd0c729a705f0186b0d2d9b
Wanderley Guimarães da Silva <wanderley.guimaraes@gmail.com>
2017-02-26 20:10:06 -0800

    Extract the format logic of date and currency to functions

--- src/Views/Table.elm
+++ src/Views/Table.elm
@@ -1,22 +1,30 @@
 module Views.Table exposing (view)
 
+import Date exposing (Date)
 import Date.Extra as DateE
 import Html exposing (Html, text, div, img)
 import Material.Table as Table
 import Model exposing (ExpenseItem)
 import Msg exposing (Msg)
+import FormatNumber
+import FormatNumber.Locales
 
+formatCurrency : Float -> String
+formatCurrency =
+  FormatNumber.format FormatNumber.Locales.usLocale
+
+formatDate : Date -> String
+formatDate =
+  DateE.toFormattedString "M/d/y h:mm a"
 
 itemView : ExpenseItem -> Html Msg
 itemView item =
     Table.tr []
         [ Table.td [] [ text item.source ]
-        , Table.td []
-            [ text <| DateE.toFormattedString "M/d/y h:mm a" item.date ]
+        , Table.td [] [ text <| formatDate item.date ]
         , Table.td [] [ text item.description ]
         , Table.td [] [ text item.category ]
-        , Table.td [ Table.numeric ] [ text <| toString item.amount ]
-        ]
+        , Table.td [ Table.numeric ] [ text <| formatCurrency item.amount ] ]
 
 
 view : List ExpenseItem -> Html Msg
