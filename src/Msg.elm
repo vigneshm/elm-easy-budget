86db2ddc04969b49f6cc3e58c8b8a884e9af117d
Wanderley Guimarães da Silva <wanderley.guimaraes@gmail.com>
2017-03-01 23:24:47 -0800

    Add new expense using static data

--- src/Msg.elm
+++ src/Msg.elm
@@ -1,17 +1,21 @@
 module Msg exposing (..)
 
 import Material
-import Model exposing (Model)
+import Model exposing (Model, ExpenseItem)
 
 
 type Msg
     = Noop
+    | AddNewExpense ExpenseItem
     | Mdl (Material.Msg Msg)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        AddNewExpense expense ->
+            ( { model | expenses = model.expenses ++ [ expense ] }, Cmd.none )
+
         Noop ->
             ( model, Cmd.none )
 
